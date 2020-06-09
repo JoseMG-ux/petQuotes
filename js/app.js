59a78851b811f8e638f7c0eca27f2890933a4a69
Jose Moreno Gomez <joseutp14@gmail.com>
2020-06-08 23:37:53 -0600

    declaration of variables

---
+++ js/app.js
@@ -0,0 +1,12 @@
+let DB;
+
+//Selectores de la interfaz
+const formulario = document.querySelector('form'),
+     mascota = document.querySelector('#mascota'),
+     nombreCliente = document.querySelector('#cliente'),
+     telefono = document.querySelector('#telefono'),
+     fecha = document.querySelector('#fecha'),
+     hora = document.querySelector('#hora'),
+     sintomas = document.querySelector('#sintomas'),
+     citas = document.querySelector('#citas'),
+     heacdingAdministra = document.querySelector('#administra')
