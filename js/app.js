5563bbb2afad6a67f5544fb3195e8399c8453b36
Jose Moreno Gomez <joseutp14@gmail.com>
2020-06-08 23:48:03 -0600

    create DB

--- js/app.js
+++ js/app.js
@@ -9,4 +9,24 @@ const formulario = document.querySelector('form'),
      hora = document.querySelector('#hora'),
      sintomas = document.querySelector('#sintomas'),
      citas = document.querySelector('#citas'),
-     heacdingAdministra = document.querySelector('#administra')
+     heacdingAdministra = document.querySelector('#administra');
+
+
+//Esperar por el DOM Ready
+document.addEventListener('DOMContentLoaded', () => {
+     //crear la base de datos
+     let crearDB = window.indexedDB.open('citas', 1);//nombre y version, siempre usar numero enteros
+
+     //Si hay 1 error enviarlo a la consola
+     crearDB.onerror = function(){
+          console.log('hubo un error')
+     }
+     //Si todo esta bien entonces mostrar en consola y asignar la base de datos
+     crearDB.onsuccess = function(){
+          console.log('todo listo');
+
+          //Asignar a la base de datos
+          DB = crearDB.result;
+          console.log(DB)
+     }
+});
